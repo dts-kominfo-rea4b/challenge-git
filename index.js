ac906f760cb28e7fa8fa74654be9be14c0e36243
zidhaanfa <zidhaanb@gmail.com>
2022-09-14 21:51:54 +0700

    Perkenalan diri

---
+++ index.js
@@ -0,0 +1,6 @@
+<body>
+
+<p>Halo, Namaku Muhamad Zidan Fathoni dengan id: 1532649113101-196, aku bekerja di breezelabs.id. Aku menjadi seorang Full-Stack Developer menggunakan Flutter, python(0d00), sedikit menggunakan vue.js, adonis.js. Tapi aku masih bingung dengan javascript, maka dari itu aku mengikuti program ini untuk mendalami pengetahuan mengenai javascript menggunakan React</p>
+<p>Aku memiliki website portfolio yaitu "https://zidanfath.tech" menggunakan flutter "https://flutter.zidanfath.tech" dan vue.js https://vue.zidanfath.tech, walaupun vue.js masih menggunakan template orang lain yang aku rubah sendiri. program ini nantinya akan mengajarkan saya membuat project sendiiri dari awal</p>
+
+</body>
